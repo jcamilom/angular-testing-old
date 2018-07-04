ef61222ba6265af5e24eefaa5a25d1a618f3b94d
jcamilom <jcamilomunozz@gmail.com>
2018-07-04 15:56:02 -0500

    Using beforeEach to provide auto-assert

--- src/app/calculator.spec.ts
+++ src/app/calculator.spec.ts
@@ -1,12 +1,17 @@
 import { Calculator } from './calculator';
 
 describe('Test for Calculator', () => {
+
+    let calculator;
+
+    // Arrange
+    beforeEach(() => {
+        calculator = new Calculator();
+    });
     
     describe('Test for multiply', () => {
 
         it('should return nine', () => {
-            // Arrange
-            let calculator = new Calculator();
             // Act
             let result = calculator.multiply(3, 3);
             // Assert
@@ -14,8 +19,6 @@ describe('Test for Calculator', () => {
         });
 
         it('should return four', () => {
-            // Arrange
-            let calculator = new Calculator();
             // Act
             let result = calculator.multiply(1, 4);
             // Assert
@@ -27,16 +30,12 @@ describe('Test for Calculator', () => {
     describe('Test for divide', () => {
 
         it('divide for a number', () => {
-            // Arrange
-            let calculator = new Calculator();
             // Act && Assert
             expect(calculator.divide(6, 3)).toEqual(2);
             expect(calculator.divide(5, 2)).toEqual(2.5);
         });
 
         it('divide by zero', () => {
-            // Arrange
-            let calculator = new Calculator();
             // Act && Assert
             expect(calculator.divide(4, 0)).toBeNull;
             expect(calculator.divide(1234, 0)).toBeNull;
