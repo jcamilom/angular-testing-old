2414042e3a38daff2dc4ee607842377d51a9e380
jcamilom <jcamilomunozz@gmail.com>
2018-07-04 11:46:30 -0500

    Manual testing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Calculator } from './calculator';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,17 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+
+  ngOnInit() {
+    let calculator = new Calculator();
+    let result = calculator.multiply(3, 3);
+    console.log(result === 9);
+    console.log(result !== 12);
+
+    let result2 = calculator.divide(6, 2);
+    console.log(result2 === 3);
+    console.log(result2 !== 34);
+
+  }
+
 }
---
+++ src/app/calculator.ts
@@ -0,0 +1,9 @@
+export class Calculator {
+    multiply(numberA: number, numberB: number): number {
+        return numberA * numberB;
+    }
+
+    divide(numberA: number, numberB: number): number {
+        return numberA / numberB;
+    }
+}
