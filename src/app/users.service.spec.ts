85ccbbdbe138a0283c3a5c3f0ccf1a855943284e
jcamilom <jcamilomunozz@gmail.com>
2018-07-06 10:32:23 -0500

    Test for getUser when server is offline

--- src/app/users.service.spec.ts
+++ src/app/users.service.spec.ts
@@ -84,6 +84,58 @@ describe('UsersService', () => {
       }))
     );
 
+    it('shouldn\'t return the user\'s data when the server fails',
+      inject([UsersService, MockBackend], fakeAsync((usersService, mockBackend: MockBackend) => {
+
+        // Arrange
+        let dataResponse, dataError;
+        const userMock = {
+          'id': 1,
+          'name': 'Leanne Graham',
+          'username': 'Bret',
+          'email': 'Sincere@april.biz',
+          'address': {
+            'street': 'Kulas Light',
+            'suite': 'Apt. 556',
+            'city': 'Gwenborough',
+            'zipcode': '92998-3874',
+            'geo': {
+              'lat': '-37.3159',
+              'lng': '81.1496'
+            }
+          },
+          'phone': '1-770-736-8031 x56442',
+          'website': 'hildegard.org',
+          'company': {
+            'name': 'Romaguera-Crona',
+            'catchPhrase': 'Multi-layered client-server neural-net',
+            'bs': 'harness real-time e-markets'
+          }
+        };
+        const mockResponse = new ResponseOptions({body: JSON.stringify(userMock)});
+        mockBackend.connections.subscribe(connection => {
+          expect(connection.request.url).toBe('http://jsonplaceholder.typicode.com/users/1');
+          connection.mockError(new Error('error'));
+        });
+
+        // Act
+        usersService.getUser(1)
+          .subscribe(
+            resp => { // Success
+              dataResponse = resp;
+            }, error => { // Error
+              dataError = error;
+            }
+          );
+        tick();
+
+        // Assert
+        expect(dataResponse).toBeUndefined();
+        expect(dataError).toBeDefined();
+
+      }))
+    );
+
   });
 
 });
