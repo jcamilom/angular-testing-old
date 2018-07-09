95ecacd883be7bc462fae3df0336d6ec94b64c19
jcamilom <jcamilomunozz@gmail.com>
2018-07-09 13:19:53 -0500

    Fix test for AppComponent

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,10 +1,14 @@
 import { TestBed, async } from '@angular/core/testing';
 import { AppComponent } from './app.component';
+
+import { PersonRowComponent } from './person-row/person-row.component';
+
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [
-        AppComponent
+        AppComponent,
+        PersonRowComponent
       ],
     }).compileComponents();
   }));
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { Calculator } from './calculator';
 
+import { Person } from './person';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -8,6 +10,18 @@ import { Calculator } from './calculator';
 })
 export class AppComponent {
   title = 'ing-test';
+
+  person: Person;
+
+  constructor() {
+    this.person = new Person(
+      'Andres',
+      'Paniagua',
+      19,
+      70,
+      1.70
+    );
+  }
 
   ngOnInit() { }
 
